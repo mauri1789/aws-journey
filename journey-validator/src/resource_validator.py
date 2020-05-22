311bda788f6884c5a9029cb5f5150b5cd89b1a61
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2020-05-22 22:18:00 +0000

    added queue for resource validator

--- journey-validator/src/resource_validator.py
+++ journey-validator/src/resource_validator.py
@@ -2,6 +2,8 @@ import boto3
 import json
 
 def handler(event, context):
+    print("-========-")
+    print(event)
     return {
         "statusCode": 200,
         "body": json.dumps("lambda :)")
--- journey-validator/src/step_validator.py
+++ journey-validator/src/step_validator.py
@@ -1,9 +1,16 @@
+import os
 import json
+import uuid
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
+table = os.getenv('TABLE_NAME')
+queue = os.getenv('QUEUE_NAME')
+
 dynamodb = boto3.resource('dynamodb')
-journey_table = dynamodb.Table('journey-app-table')
+sqs = boto3.resource('sqs')
+journey_queue = sqs.get_queue_by_name(QueueName=queue)
+journey_table = dynamodb.Table(table)
 
 def handler(event, context):
     body = json.loads(event["body"])
@@ -13,12 +20,18 @@ def handler(event, context):
     
     steps = get_lab_data(lab)
     
-    for step in steps:
-        print(step, user_input)
+    queue_entries = [
+        {
+            "Id": str(uuid.uuid4()),
+            "MessageBody": step
+        } for step in steps
+    ]
+    
+    journey_queue.send_messages(Entries=queue_entries)
     
     return {
         "statusCode": 200,
-        "body": json.dumps("Success")
+        "body": json.dumps({"queue_entries": queue_entries})
     }
 
 def get_lab_data(lab):
