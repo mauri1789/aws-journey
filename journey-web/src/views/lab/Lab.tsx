08ca1a6d648c96e74b0a532dc3d8144ef0dccc67
Mauricio Alarcon <mauricio.alarcon@coxautoinc.com>
2020-05-29 20:37:47 -0400

    added section headers and toggleSection for headers

--- journey-web/src/views/lab/Lab.tsx
+++ journey-web/src/views/lab/Lab.tsx
@@ -11,51 +11,78 @@ import {
   useParams
 } from "react-router-dom";
 
-interface Section {
+interface ISection {
   section: string;
   title: string;
   description: string;
 }
-interface Lab {
+interface ILab {
   description: string;
 }
 
 function Lab() {
-  const [lab, setLab] = useState<Lab>();
-  let [sections, setSections] = useState<Section[]>([]);
-  useEffect(() => {
-    let get_journey = async () => {
-      let response = await axios.get(journey_url('content', 'lab/S3_static_website'))
-      let { data } = response;
-      setLab(data["lab"])
-      setSections(data["sections"])
-    }
-    get_journey();
-  },[])
+   const [lab, setLab] = useState<ILab>();
+   let [sections, setSections] = useState<ISection[]>([]);
+   useEffect(() => {
+      let get_journey = async () => {
+         let response = await axios.get(journey_url('content', 'lab/S3_static_website'))
+         let { data } = response;
+         setLab(data["lab"])
+         setSections(data["sections"])
+      }
+      get_journey();
+   },[])
 
-  let section_group = sections.map((section) => 
-    <div className="section">
-      <div className="section-header">
-        <div className="section-open">
-          <FontAwesomeIcon icon={faCaretRight} />
-        </div>
-        <div className="section-title">
-          {section!.title}
-        </div>
-      </div>
-    </div>
-  )
-
-  let {lab_id, topic_id} = useParams()
-    return (
+   let {lab_id, topic_id} = useParams()
+   return (
       <div className="lab">
-        <h1>S3 Static Website</h1>
-        <p>{lab?.description}</p>
-        <div className="sections">
-          {section_group}
-        </div>
+         <h1>S3 Static Website</h1>
+         <p>{lab?.description}</p>
+         <div className="sections">
+            {sections.map((section, index) =>
+               <Section section={section} index={index} key={index} />
+            )}
+         </div>
       </div>
-    );
-  }
-  
-  export default Lab;
+   );
+}
+interface ISectionProps {
+   section: ISection;
+   index: number;
+}
+function Section ({section, index}:ISectionProps) {
+   const first_section = (index==0)?true:false
+   const [sectionOpen, setSectionOpen] = useState<boolean>(first_section);
+   let toggleSection = () => setSectionOpen(!sectionOpen)
+   return (
+      <div className="section">
+         <SectionHeader
+            title={section!.title}
+            toggleSection={toggleSection}
+         />
+      </div>
+   )
+}
+
+interface ISectionHeaderProps {
+   title: string;
+   toggleSection: () => void
+}
+function SectionHeader ({title, toggleSection}:ISectionHeaderProps) {
+   return (
+      <div className="section-header">
+         <div className="section-open" onClick={toggleSection}>
+            <FontAwesomeIcon icon={faCaretRight} />
+         </div>
+         <div className="section-title" onClick={toggleSection}>
+            {title}
+         </div>
+      </div>
+   )
+}
+
+
+
+
+
+export default Lab;
