0e7865e0125aa816c4775a0394f129b9607a685f
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2020-06-17 22:02:56 -0400

    added storage icon

--- journey-web/src/views/journey/Journey.tsx
+++ journey-web/src/views/journey/Journey.tsx
@@ -1,12 +1,215 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './Journey.scss';
+import { Lines } from './drawings/Lines'
+import { TopicDrawing, TOPIC_IN_PROGRESS, TOPIC_UNDONE, TOPIC_DONE } from './drawings/Topic';
+import { BucketDrawing } from './drawings/Bucket'
 
 function Journey() {
-    return (
+   let topics = [
+      {
+         title: "Fundamentals",
+         x: "325.5",
+         y: "26.5",
+         status: TOPIC_DONE,
+         selected: true,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Traditional Computing",
+         x: "75.5",
+         y: "251.5",
+         status: TOPIC_DONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Serverless Compute",
+         x: "325.5",
+         y: "251.5",
+         status: TOPIC_DONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Object Storage",
+         x: "575.5",
+         y: "251.5",
+         status: TOPIC_DONE,
+         selected: false,
+         type: "chapter",
+         icon: (container:ReactNode) => <BucketDrawing container={container}/>
+      },
+      {
+         title: "Kinesis",
+         x: "125.5",
+         y: "476.5",
+         status: TOPIC_DONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "DynamoDB",
+         x: "325.5",
+         y: "476.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Excel Import",
+         x: "525.5",
+         y: "476.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Relational Databases",
+         x: "75.5",
+         y: "701.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Security",
+         x: "325.5",
+         y: "701.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Accessing the cloud",
+         x: "575.5",
+         y: "701.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Messaging",
+         x: "125.5",
+         y: "926.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "CloudFormation",
+         x: "325.5",
+         y: "926.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Serverless App",
+         x: "525.5",
+         y: "926.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Elastic BeanStalk",
+         x: "75.5",
+         y: "1151.5",
+         status: TOPIC_IN_PROGRESS,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "CF Serverless App",
+         x: "325.5",
+         y: "1151.5",
+         status: TOPIC_UNDONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "User Management",
+         x: "575.5",
+         y: "1151.5",
+         status: TOPIC_UNDONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "Monitoring",
+         x: "125.5",
+         y: "1376.5",
+         status: TOPIC_UNDONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "CI/CD",
+         x: "525.5",
+         y: "1376.5",
+         status: TOPIC_UNDONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "DynamoDB Cache",
+         x: "225.5",
+         y: "1601.5",
+         status: TOPIC_UNDONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      },
+      {
+         title: "CI/CD Project",
+         x: "425.5",
+         y: "1601.5",
+         status: TOPIC_UNDONE,
+         selected: false,
+         type: "chapter",
+         icon: null
+      }
+   ]
+   return (
       <div className="journey">
-        Journey
+         <div className="map">
+            <svg viewBox="0 0 800 1800">
+               {topics.map((topic, index) =>
+                  <TopicDrawing
+                     x={+topic.x}
+                     y={+topic.y}
+                     status={topic.status}
+                     selected = {topic.selected}
+                     text={topic.title}
+                     icon={topic.icon}
+                     type={topic.type}
+                  />
+               )}
+               <Lines />
+            </svg>
+         </div>
+         <div className="topic">
+         topic
+         </div>
       </div>
-    );
+   );
   }
   
   export default Journey;
---
+++ journey-web/src/views/journey/drawings/Topic.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from 'react';
+import Topic from '../../topic/Topic';
+import './Topic.scss';
+let TOPIC_DONE = "DONE"
+let TOPIC_IN_PROGRESS = "IN_PROGRESS"
+let TOPIC_UNDONE = "UNDONE"
+
+interface TopicDrawingProps {
+    x: number
+    y: number
+    status: string
+    selected: boolean
+    text: string
+    type: string
+    icon: ((container: ReactNode)=>JSX.Element) | null
+  }
+ 
+function TopicDrawing ({x, y, status, selected, text, icon, type}: TopicDrawingProps) {
+    let side = 149
+    let titleHeight = 35
+    let groupClass = "chapter-topic"
+    if ( type == "project" ) {
+        groupClass = "project-topic"
+    }
+    let Container= () => (
+        <g className={groupClass}>
+            <rect x={x} y={y} className="topic-cont " width={side} height={side}/>
+            <rect x={x} y={y+114} className="topic-title-cont " width={side} height={titleHeight}/>
+            <text 
+                x={x + side / 2}
+                y={y + (side-titleHeight) + titleHeight / 2}
+                dominantBaseline="middle"
+                textAnchor="middle"
+                className="topic-title">
+                    {text}
+            </text>
+        </g>
+    )
+    return (
+        <g className={(selected)?"active-topic":""} >            
+            {icon && icon(<Container />)}
+        </g>
+    )
+}
+
+export { TopicDrawing, TOPIC_DONE, TOPIC_IN_PROGRESS, TOPIC_UNDONE }
